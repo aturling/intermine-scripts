cf4e297941921762973ff2c4278f8d25715ed907
Amy Walsh <walshamy@missouri.edu>
2021-06-28 16:55:54 -0500

    Fixed variables for v4 script

--- common/script_vars_maizemine1.4.sh
+++ common/script_vars_maizemine1.4.sh
@@ -3,18 +3,17 @@
 # Variables common to intermine scripts
 run_datetime=`date +%Y%m%d%H%M`
 
-mine_name="hymenopteramine"
+mine_name="maizemine"
 script_base=`basename "$0"`
 script_name=${script_base%.*}
 log_dir="$HOME/intermine-scripts/loading_v4/log"
 script_outfile="${log_dir}/${mine_name}_${script_name}_${run_datetime}.out"
 
-mine_base_dir="/db/hymenopteramine_v1.5"
+mine_base_dir="/db/maizemine_v1.4"
 mine_logs_dir="${mine_base_dir}/logs"
 build_dir="${mine_logs_dir}/build_out"
-mine_home_dir="${mine_base_dir}/intermine/hymenopteramine"
+mine_home_dir="${mine_base_dir}/intermine/maizemine"
 
 mail_subj="Intermine script"
 mail_msg="Intermine script ${script_base}"
 notify_email="walshamy@missouri.edu"
-
