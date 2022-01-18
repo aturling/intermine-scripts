88582ae82b5759d8a208206bac25959aa46ad309
Amy Walsh <walshamy@missouri.edu>
2022-01-18 15:20:28 -0600

    Small update to test script for v4+

--- common/check_files_and_dirs_in_projectxml.sh
+++ common/check_files_and_dirs_in_projectxml.sh
@@ -23,7 +23,7 @@ variablesfile=$1
 . $variablesfile
 
 # location of project.xml file
-projectxmlfile="${interminedir}/project.xml"
+projectxmlfile="${mine_home_dir}/project.xml"
 
 # call perl script that parses project.xml and gets list of directory and filenames
 perl get_files_and_dir_names.pl $projectxmlfile > tmpnames.txt
@@ -41,8 +41,6 @@ while read name; do
         if [ ! -s "$name" ]; then
             echo "Warning: file $name exists but is empty!"
         fi
-    elif [ -f "${integratedir}/$name" ]; then
-        echo "NOTE: file $name exists but path is relative (to ${integratedir})"
     else
         echo "WARNING: $name does not exist"
     fi
