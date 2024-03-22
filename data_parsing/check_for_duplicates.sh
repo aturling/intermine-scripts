e268e2f6eff6e98cbe6634318e2d4ab877231f48
Amy Walsh <aturling@users.noreply.github.com>
2024-03-22 16:03:53 -0500

    Fixed logic error and added use of parallel in duplicate check script

--- data_parsing/check_for_duplicates.sh
+++ data_parsing/check_for_duplicates.sh
@@ -2,6 +2,12 @@
 
 # Check for duplicates in sources where duplicate ids would cause a loading error
 
+# Check for parallel... really slow without it
+if ! command -v parallel >/dev/null 2>&1; then
+    echo "GNU Parallel not installed!"
+    exit 1
+fi
+
 # Ensembl Compara / Biomart symbols and descriptions
 
 if [[ $(find /db/*/datasets -type d -name ensembl-plant-biomart) ]]; then
@@ -27,9 +33,12 @@ fi
 # Iterate over all GFF files
 echo "Checking gff files..."
 dupes_found=0
-gff_dirs=$(find /db/*/datasets/ -type f -name "*.gff*" -exec dirname {} \;)
+gff_dirs=$(find /db/*/datasets/ -type f -name "*.gff*" -exec dirname {} \; | sort | uniq -u)
 for gff_dir in $gff_dirs; do
-    num_dupes=$(cat $gff_dir/*gff* | cut -f9 | grep -oE '[^_]?ID=[^;]*' | sort | uniq -c | awk '{print $1}' | grep -vE '^1$' | wc -l)
+    #echo "checking $gff_dir"
+    num_dupes=$(parallel --no-notice --pipepart -a $gff_dir/*.gff* --block -1 LC_ALL=C grep -oE '[^_]?ID=[^\;]*' | sort -S 10% | uniq -d | wc -l)
+    #too slow:
+    #num_dupes=$(cat $gff_dir/*gff* | cut -f9 | grep -oE '[^_]?ID=[^;]*' | sort | uniq -c | awk '{print $1}' | grep -vE '^1$' | wc -l)
     if [ "$num_dupes" -gt 0 ]; then
         echo "WARNING: Duplicate ids found in gff dir: $gff_dir"
         dupes_found=1
