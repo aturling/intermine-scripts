248daed60bc25d299424d5fa954a1bd4efd198e8
Amy Walsh <aturling@users.noreply.github.com>
2024-08-08 14:07:58 -0500

    Fixed logic error in OrthoDB check script

--- data_parsing/check_orthodb_files.sh
+++ data_parsing/check_orthodb_files.sh
@@ -18,7 +18,7 @@ echo
 # Check for missing taxon ids
 echo "Checking for missing taxon IDs..."
 # Should return 0 (i.e., no taxon ids missing):
-num_missing_taxon_ids=$(awk -F'\t' '{print $6}' ${dirname}/* | sort | uniq | grep -vE '[0-9]+' | wc -l)
+num_missing_taxon_ids=$(awk -F'\t' '{print $6}' ${dirname}/*.tab | sort | uniq | grep -vE '[0-9]+' | wc -l)
 if [ "$num_missing_taxon_ids" -ne 0 ]; then
     echo "WARNING: At least one file in $dirname has missing taxon IDs" 1>&2
     ec=1
@@ -30,8 +30,8 @@ echo
 # Check for singleton clusters
 echo "Checking for singleton clusters..."
 # Should not return anything:
-has_singleton_clusters=$(awk -F'\t' '{print $2}' ${dirname}/* | uniq -c | sort -n | awk '{print $1}' | uniq -c | head -n 1 | grep ' 1')
-if [ ! -z "$has_singleton_clusters" ]; then
+has_singleton_clusters=$(awk -F'\t' '{print $2}' ${dirname}/*.tab | uniq -c | sort -n | awk '{print $1}' | uniq -c | awk '{print $2}' | head -n 1)
+if [ "$has_singleton_clusters" -lt 2 ]; then
    echo "WARNING: At least one file in $dirname has singleton cluster(s)" 1>&2
    ec=1
 else
@@ -42,7 +42,7 @@ echo
 # Check for duplicates within a cluster/LCA
 echo "Checking for duplicates within a cluster and LCA..."
 # Should return 1 (i.e., all unique, no duplicates) 
-num_duplicates=$(awk -F'\t' '{print $2 "|" $3 "|" $7 }' ${dirname}/* | sort | uniq -c | awk '{print $1}' | sort -n | uniq -c | wc -l)
+num_duplicates=$(awk -F'\t' '{print $2 "|" $3 "|" $7 }' ${dirname}/*.tab | sort | uniq -c | awk '{print $1}' | sort -n | uniq -c | wc -l)
 if [ "$num_duplicates" -ne 1 ]; then
     echo "WARNING: At least one file in $dirname contains duplicates within a cluster and LCA" 1>&2
     ec=1
